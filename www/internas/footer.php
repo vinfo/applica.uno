55a983af45feb33b0622b5e9c857c59e42971429
VICTOR VALENCIA <victor.valencia@gmail.com>
2017-04-27 16:46:42 -0500

    Carga Inicial

---
+++ www/internas/footer.php
@@ -0,0 +1,53 @@
+<div class="container-fluid footer">
+		<div class="container">
+			<div class="col-md-6">
+				<p>Subscripción a nuestro Boletin:</p>
+				<form>
+					<input type="text" ><button>Enviar</button>
+				</form>
+				<img src="images/msgfoot.png" alt="" title="">
+			</div>
+			<div class="col-md-6 txt">
+				<div class="col-md-4">
+					<label>Productos</label>
+					<ul>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<label>Delicias fruture</label>
+					<ul>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<label>Servicios b2b</label>
+					<ul>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+						<li><a href="">Categoria 1</a></li>
+					</ul>
+				</div>
+			</div>
+			<hr>
+			<div class="concopy">
+				&copy; FRUTURE FOODS 2015
+				<div class="masinfo">
+					 +4(000) 000 0000 <span>|</span> SHOP@FRUTUREFOODS.COM
+				</div>
+			</div>
+		</div>
+	</div>
