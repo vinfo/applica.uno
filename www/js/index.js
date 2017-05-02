acd08d4e902869cced43fd64837c19af6229ee69
VICTOR VALENCIA <victor.valencia@gmail.com>
2017-05-02 11:26:06 -0500

    app

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        loadTemplate("nueva_inicio_sesion_personas");
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
