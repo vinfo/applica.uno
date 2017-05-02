82f098d43d91da666ace86f459413e65725d43f5
VICTOR VALENCIA <victor.valencia@gmail.com>
2017-05-02 12:09:02 -0500

    Offline

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        checkConnection();
         $("#centralContent").load("http://applica.uno/mobil/nueva_inicio_sesion_personas.php", function () {
             console.log("Contenido cargado exitosamente.");
         });
@@ -43,3 +44,46 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+function getDeviceProperty()
+{
+     console.log("getDeviceProperty");
+     var deviceOS  = device.platform  ;  //fetch the device operating system
+     var deviceOSVersion = device.version ;  //fetch the device OS version
+     var uuid=  device.uuid;
+     localStorage.setItem("OS",deviceOS);
+     localStorage.setItem("UUID",uuid);
+     console.log("Plataforma registrada "+device.platform);
+}
+function checkConnection() {
+    console.log("checkConnection");
+    var state=true;
+    var networkState = navigator.connection.type;
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection'; 
+    
+    var page=getNameURLWeb();
+    if(states[networkState]=='No network connection'){
+        //navigator.notification.beep(1);        
+        if(page!="offline.html"){
+            alert('Internet es requerido!');        
+            window.location.href = 'offline.html';
+        }
+        //throw new Error('No Internet Connection.');  
+        state=false;                            
+    }else{
+        if(page=="offline.html")window.location.href = 'index.html';
+    }
+    return state;    
+}
+function getNameURLWeb(){
+ var sPath = window.location.pathname;
+ var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
+ return sPage;
+}
