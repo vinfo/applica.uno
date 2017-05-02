0c7294909392054df1340754ec6fb57620d7c874
VICTOR VALENCIA <victor.valencia@gmail.com>
2017-05-02 15:46:57 -0500

    Offline

--- www/js/offline.js
+++
@@ -1,5 +0,0 @@
-var checkCon= setInterval(function () {	
-	if(localStorage.offline=="true")checkConnection();
-}, 5000);
-alert(localStorage.offline);
-if(localStorage.offline=="false")clearInterval(checkCon);
