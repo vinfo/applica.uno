6bd226268dbb8e63f581874ddeef8ec840dacdfc
VICTOR VALENCIA <victor.valencia@gmail.com>
2017-05-02 11:05:27 -0500

    app

--- www/js/app.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $("#centralContent").load("http://applica.uno/mobil/nueva_inicio_sesion_personas.php", function () {
-        console.log("Contenido cargado exitosamente.");
-    });
-});
