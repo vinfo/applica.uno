ad6e4cd316a1dd84184b93fc884a5798c524d709
VICTOR VALENCIA <victor.valencia@gmail.com>
2017-05-12 06:56:42 -0500

    var

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var historyBack = [];
 var app = {
     // Application Constructor
     initialize: function () {
