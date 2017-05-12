f9b48726247393697650ff065a5eb9b11215c347
VICTOR VALENCIA <victor.valencia@gmail.com>
2017-05-11 21:02:50 -0500

    Back

--- www/js/index.js
+++ www/js/index.js
@@ -86,6 +86,16 @@ function getNameURLWeb() {
     return sPage;
 }
 function onBackKeyDown() {
-    history.go(-1);
-    navigator.app.backHistory();
+    alert("back");
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i)) {
+        // IOS DEVICE
+        history.go(-1);
+    } else if (userAgent.match(/Android/i)) {
+        // ANDROID DEVICE
+        navigator.app.backHistory();
+    } else {
+        // EVERY OTHER DEVICE
+        history.go(-1);
+    }
 }
